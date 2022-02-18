f75934c18e1d4423ba4d724d136ee08e9eb94a47
Balaji Narayana <85923874+BALAJI141031@users.noreply.github.com>
2022-02-18 18:30:53 +0000

    syntax is changed to es-6

--- index.js
+++ index.js
@@ -7,22 +7,26 @@ const squareBy5=num=>num**5
 const divideby9=num=>num/9
 
 
-function compose(...params) {
-   console.log(params)
-  return function (num) {
-    const reductionresult=params.reduce(function (acc,cv) { 
-      if( typeof acc !=="function"){
-       return cv(acc)
-      }else{       
-        return cv(acc(num))
-    }
+// function compose(...params) {
+//    console.log(params)
+//   return function (num) {
+//     const reductionresult=params.reduce(function (acc,cv) { 
+//       if( typeof acc !=="function"){
+//        return cv(acc)
+//       }else{       
+//         return cv(acc(num))
+//     }
       
-    })
+//     })
 
-    return reductionresult
+//     return reductionresult
     
-  }  
-}
+//   }  
+// }
+
+// es-6 syntax
+ 
+const compose=(...params)=> num=>params.reduce(reducer=(acc,cv)=> typeof acc !=="function"?cv(acc):cv(acc(num)))
 
 let result=compose(increase,multiplywithNum,squareBy5,divideby9)
 
